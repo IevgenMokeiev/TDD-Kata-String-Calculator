0c40706db4118322f02a838187eeb1fda14e3d9d
Ievgen Mokeiev <ievgen.mokeiev@just-eat.com>
2020-05-14 18:06:52 +0300

    tdd - step 1 done

--- StringCalculatorApp/StringCalculatorAppTests/StringCalculatorAppTests.swift
+++ StringCalculatorApp/StringCalculatorAppTests/StringCalculatorAppTests.swift
@@ -9,10 +9,28 @@
 import XCTest
 @testable import StringCalculatorApp
 
+class StringCalculator {
+    func add(numbers: String) -> Int {
+        let components = numbers.components(separatedBy: ",")
+        return components.reduce(0) { $0 + (Int($1) ?? 0) }
+    }
+}
+
 class StringCalculatorAppTests: XCTestCase {
 
-    func test_add_two_number() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test0empty_string() {
+        XCTAssertTrue(makeSUT().add(numbers: "") == 0)
+    }
+
+    func test_1_number_string() {
+        XCTAssertTrue(makeSUT().add(numbers: "1") == 1)
+    }
+
+    func test_2_number_string() {
+        XCTAssertTrue(makeSUT().add(numbers: "1,2") == 3)
+    }
+
+    func makeSUT() -> StringCalculator {
+        return StringCalculator()
     }
 }
