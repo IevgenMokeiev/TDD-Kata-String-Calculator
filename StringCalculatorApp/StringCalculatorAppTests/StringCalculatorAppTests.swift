5fdabbdb6ec3793cc3a829170f014b4dcce62d46
Ievgen Mokeiev <ievgen.mokeiev@just-eat.com>
2020-05-14 17:45:46 +0300

    added repo

---
+++ StringCalculatorApp/StringCalculatorAppTests/StringCalculatorAppTests.swift
@@ -0,0 +1,18 @@
+//
+//  StringCalculatorAppTests.swift
+//  StringCalculatorAppTests
+//
+//  Created by Yevhen Mokeiev on 14.05.2020.
+//  Copyright © 2020 Yevhen Mokeiev. All rights reserved.
+//
+
+import XCTest
+@testable import StringCalculatorApp
+
+class StringCalculatorAppTests: XCTestCase {
+
+    func test_add_two_number() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
